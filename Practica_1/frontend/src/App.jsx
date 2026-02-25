73f398f2e1089e0d16497d8befab74468d448b1b
sudokentucky <31164549240704@ingenieria.usac.edu.gt>
2026-02-24 20:34:22 -0600

    feat: agregar ruta lista de reproduccion

--- Practica_1/frontend/src/App.jsx
+++ Practica_1/frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login.jsx';
 import Registrar_Usuario from './pages/RegistroUsuario/RegistrarUsuario';
 import Cartelera from './pages/Cartelera/Cartelera';
 import Editar_Usuario from './pages/EditarUsuario/EditarUsuario.jsx';
+import ListaReproduccion from './pages/ListaReproduccion/ListaReproduccion.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/registrar-usuario" element={<Registrar_Usuario />} />
             <Route path="/cartelera" element={<Cartelera />} />
             <Route path="/editar-informacion" element={<Editar_Usuario />} />
+            <Route path="/lista-reproduccion" element={<ListaReproduccion />} />
             <Route path="*" element={<center><h1>404 Not Found</h1></center>} />
         </Routes>
       </BrowserRouter>
